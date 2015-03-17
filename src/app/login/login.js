6d5c6782463a8b1942d42295d2b1155f2d1029d7
Sigurður Atlason <sigurdura13@ru.is>
2015-03-17 00:28:27 +0000

    start-server script and fixed login html a bit

---
+++ src/app/login/login.js
@@ -0,0 +1,29 @@
+angular.module( 'ngBoilerplate.login', [
+  'ui.router',
+  'placeholders',
+  'ui.bootstrap'
+])
+
+.config(function config( $stateProvider ) {
+  $stateProvider.state( 'login', {
+    url: '/login',
+    views: {
+      "main": {
+        controller: 'LoginCtrl',
+        templateUrl: 'login/login.tpl.html'
+      }
+    },
+    data:{ pageTitle: 'Login' }
+  });
+})
+
+.controller( 'LoginCtrl', function LoginCtrl( $scope ) {
+  // This is simple a demo for UI Boostrap.
+  $scope.dropdownDemoItems = [
+    "The first choice!",
+    "And another choice for you.",
+    "but wait! A third!"
+  ];
+})
+
+;
