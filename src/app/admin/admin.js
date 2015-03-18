f5e0390b725cc0f04f1ab0850996466b1e038988
Haffi <hafthort12@ru.is>
2015-03-18 01:40:16 +0000

    admin controller in process

--- src/app/admin/admin.js
+++ src/app/admin/admin.js
@@ -21,5 +21,27 @@ evalApp.config(function config( $stateProvider ) {
 evalApp.controller( 'AdminCtrl', function AdminCtrl( $scope, $http, SERVER_URL) {
 
   console.log("inside AdminCtrl");
+  console.log($http.defaults.headers.common.Authorization);
+
+  // $scope.getTemp = function()
+  $http.get("http://dispatch.ru.is/h37/api/v1/evaluationtemplates")
+    .success (function(response) {
+      $scope.allTemplates = response;
+
+      console.log("allTemplates:");
+      console.log($scope.allTemplates);
+
+      $scope.tempTitles = [];
+
+      console.log("titles:");
+
+      console.log(response[0].Title);
+      // for (var i = 0; i < response.size(); i++) {
+      //   //$scope.tempTitles.push(i.Title);
+      //   console.log(response[i].Title);
+      // }
+
+    });
+
 
 });
