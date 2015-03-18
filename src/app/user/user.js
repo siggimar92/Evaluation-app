4408f54d5202fa53632c82022e08ad3cd4ec45c4
Sigurður Atlason <sigurdura13@ru.is>
2015-03-18 01:37:54 +0000

    display courses for specific user

--- src/app/user/user.js
+++ src/app/user/user.js
@@ -19,8 +19,24 @@ evalApp.config(function config( $stateProvider ) {
 });
 
 evalApp.controller( 'UserCtrl', function UserCtrl( $scope, $http, SERVER_URL) {
-
+  $scope.displayCourses = [];
   console.log("inside UserCtrl");
-  console.log($http.defaults.headers.common.Authorization);
+  // console.log($http.defaults.headers.common.Authorization);
+  // console.log($http.get(SERVER_URL + 'my/courses'));
+
+  $http.get(SERVER_URL + 'my/courses')
+  .success(function (data, status, headers, config) {
+    console.log("SUCCESS");
+    console.log(data);
+
+    for (var i = 0; i < data.length; i++) {
+      console.log(data[i].Name);
+      $scope.displayCourses[i] = data[i].Name;
+    }
+
+  })
+  .error(function (data, status, headers, config) {
+    console.log("ERROR");
+  });    
 
 });
