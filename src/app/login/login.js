b936daa81aa9913da18b8690d93d2aaeca1b201a
Haffi <hafthort12@ru.is>
2015-03-18 00:20:38 +0000

    factory for login added

--- src/app/login/login.js
+++ src/app/login/login.js
@@ -18,50 +18,43 @@ evalApp.config(function config( $stateProvider ) {
   });
 });
 
-evalApp.controller( 'LoginCtrl', function LoginCtrl( $scope, $http, SERVER_URL) {
+evalApp.controller( 'LoginCtrl', function LoginCtrl( $scope, $http, LoginResource, SERVER_URL, $location) {
 
   console.log("inside LoginCtrl");
-  // $scope.username = "";
-  // $scope.password = "";
+  $scope.username = "";
+  $scope.password = "";
   
-  $scope.data = {};
-  $scope.status = {};
+  // $scope.data = {};
+  // $scope.status = {};
 
   $scope.login = function(username, password) {
     console.log("button press");
 
     //Simple POST request example (passing data) :
-    $http.post(SERVER_URL + 'login', {user: $scope.username, pass: $scope.password})
-      
+    LoginResource.doLogin($scope.username, $scope.password)
       .success(function (data, status, headers, config) {
         console.log("SUCCESS");
-        $scope.data = data;
-        $scope.status = status;
+        // $scope.data = data;
+        // $scope.status = status;
+        // console.log($scope.status, $scope.data);
+
         $http.defaults.headers.common.Authorization = "Basic " + data.Token;
         console.log($http.defaults.headers.common.Authorization);
-        //$location.path('/about');
+        $location.path('/about');
       })
       .error(function (data, status, headers, config) {
         console.log("ERROR");
         console.log(status);
+        console.table(data);
       });    
   };
 });
 
-// myApp.factory('authInterceptor', function ($rootScope, $q, $window) {
-//   return {
-//     request: function (config) {
-//       config.headers = config.headers || {};
-//       if ($window.sessionStorage.token) {
-//         config.headers.Authorization = 'Bearer ' + $window.sessionStorage.token;
-//       }
-//       return config;
-//     },
-//     response: function (response) {
-//       if (response.status === 401) {
-//         // handle the case where the user is not authenticated
-//       }
-//       return response || $q.when(response);
-//     }
-//   };
-// });
+evalApp.factory('LoginResource', function ( $http, SERVER_URL) {
+  
+  return {
+    doLogin: function (username, password) {
+      return $http.post(SERVER_URL + 'login', {user: username, pass: password});
+    }
+  };
+});
