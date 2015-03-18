4f48de2a755466b2a6dff5f9a91028be54e87c6e
Haffi <hafthort12@ru.is>
2015-03-18 22:26:14 +0000

    template work

--- src/app/newTemplate/newTemplate.js
+++ src/app/newTemplate/newTemplate.js
@@ -20,7 +20,47 @@ evalApp.config(function config( $stateProvider ) {
 
 evalApp.controller( 'newTemplateCtrl', function newTemplateCtrl( $scope, $http, LoginResource, SERVER_URL, $location) {
 
-  console.log("inside newTemplateCtrl");
-  
+ console.log("inside newTemplateCtrl");
 
+  $scope.title = "";
+  $scope.titleEN = "";
+  $scope.intro = "";
+  $scope.introEN = "";
+
+  $scope.courseQ = [];
+  $scope.teacherQ = [];
+  $scope.answer = [];
+
+  console.log($scope.title);
+  console.log($scope.titleEN);
+  console.log($scope.intro);
+  console.log($scope.introEN);
+  console.log($scope.courseQ);
+  console.log($scope.teacherQ);
+
+  $scope.oneAnswer = {
+    Text: "svar",
+    TextEN: "Answer",
+    Weight: 5
+  };
+  var oneQuestion = {
+    Text: "Spurning",
+    TextEN: "Question",
+    Type: "single",
+    Answers: $scope.oneAnswer
+  };
+
+  $scope.addCQ = function() {
+    $scope.CourseQ.push();
+  };
+
+  $scope.addTQ = function() {
+    $scope.teacherQ.push();
+  };
+
+
+  $scope.createTemplate = function(title, titleEN, intro, introEN, courseQ, teacherQ) {
+  console.log("button press");
+  //Simple POST request example (passing data) :  
+  };
 });
