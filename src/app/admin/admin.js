db6a1228c30eba93a54d93dbb76d4aeabe4af202
Sigurður Atlason <sigurdura13@ru.is>
2015-03-20 02:06:04 +0000

    added error message in newTemplate

--- src/app/admin/admin.js
+++ src/app/admin/admin.js
@@ -50,12 +50,11 @@ evalApp.controller( 'AdminCtrl', function AdminCtrl( $scope, $http, SERVER_URL, 
     }
   });    
 
-  $scope.startDate = function() {
+  $scope.startDate = function () {
     var dateIso = new Date();
     console.log(dateIso.toISOString());
     //console.log(dateTime.toISOString()); 
   };
-
 
   // // $scope.getTemp = function()
   // $http.get("http://dispatch.ru.is/h37/api/v1/evaluationtemplates")
